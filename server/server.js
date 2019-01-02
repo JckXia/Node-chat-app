85a6f881a874809b5e08ba9ac850709f11946527
JackXiaisagreatguy <31197636+JackXiaisagreatguy@users.noreply.github.com>
2019-01-02 17:26:25 -0500

    Added connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const path=require('path');
+const http=require('http');
 const express=require('express');
+const socketIO=require('socket.io');
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server); //Websocket server COmmunicate between server and the client
+
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT||3000;
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+  //Similar to the client side socket
+   console.log('New user connected');
+
+   socket.on('disconnect',()=>{
+     console.log('User disconnected');
+   });
+});
+
+server.listen(port,()=>{
   console.log(`Server up on port ${port}`);
 });
 
